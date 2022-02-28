69c52eb3d8a3add3a24187a859a5581dcf5c2d09
Saad4123 <96721960+Saad4123@users.noreply.github.com>
2022-02-28 02:54:45 -0800

    Add files via upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -121,7 +121,33 @@
     });
   }
 
+function myGeeks() {
+					$('#response').html('Your Response has been recorded')
+					}
+$('#contact').submit(function(e){
+   e.preventDefault();
+   myGeeks()
+   // Code goes here
+getData(); // your onclick function call here
+
+});
+
 
 
 
 })(window.jQuery);
+
+$(function() {
+  $('#WAButton').floatingWhatsApp({
+    phone: '+92 ', //WhatsApp Business phone number International format-
+    //Get it with Toky at https://toky.co/en/features/whatsapp.
+    headerTitle: 'Chat with us on WhatsApp!', //Popup Title
+    popupMessage: 'Hello, how can we help you?', //Popup Message
+    showPopup: true, //Enables popup display
+    buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />', //Button Image
+    //headerColor: 'crimson', //Custom header color
+    //backgroundColor: 'crimson', //Custom background button color
+    position: "right"    
+  }); 
+});
+
